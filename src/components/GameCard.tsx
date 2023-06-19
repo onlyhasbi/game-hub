6e8e24b7adc5c4d9a298bcf99c4be880c0906def
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-06-20 06:50:30 +0800

    Displaying critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,20 +1,26 @@
-import { Card, CardBody, Image, Heading } from '@chakra-ui/react';
+import { Card, CardBody, Image, Heading, HStack } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
 import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
 
 interface Props {
   game: Game;
 }
 
 function GameCard({ game }: Props) {
-  const { name, background_image, parent_platforms } = game;
+  const { name, background_image, parent_platforms, metacritic } = game;
 
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={background_image} />
       <CardBody>
         <Heading fontSize="2xl">{name}</Heading>
-        <PlatformIconList platforms={parent_platforms.map((p) => p.platform)} />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
