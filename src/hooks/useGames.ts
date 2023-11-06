94c6324e4359c040baef04dafb627340997e4f68
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-11-06 10:34:55 +0800

    Refactor - simplify game query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,7 @@ const useGames = (gameQuery: GameQuery) =>
     queryFn: ({ pageParam = 1 }) =>
       apiClient.get({
         params: {
-          genres: gameQuery.genre?.id,
+          genres: gameQuery.genreId,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
